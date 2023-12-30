a6aedb6547d4a5e1a1382cab4ae20701a2216df0
nemomalila <140974435+nemomalila@users.noreply.github.com>
2023-12-30 18:50:31 +0200

    Add files via upload

---
+++ aq48roln72v12/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean' -- If that doesn't work, try 'adamant' or 'bodacious' or whatever the latest version is.
+
+game 'gta5'
+
+files {
+  'audioconfig/*.dat151.rel',
+  'audioconfig/*.dat54.rel',
+  'sfx/**/*.awc'
+}
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/aq48roln72v12_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/aq48roln72v12_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_aq48roln72v12'
